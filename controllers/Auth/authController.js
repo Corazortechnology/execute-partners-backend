e9d37576a82991712b78eafcfcfd2ac06113752b
Mantu98 <mantu07941@gmail.com>
2025-03-20 11:05:44 +0530

    fix cors error

--- controllers/Auth/authController.js
+++ controllers/Auth/authController.js
@@ -35,7 +35,7 @@ exports.signin = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    console.log(user)
+  
     if (!user || !user.password)
       return res.status(400).json({ message: "Invalid credentials" });
 
