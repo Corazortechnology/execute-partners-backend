81099001d493032ccc2b152c2eee1fbc765fd25e
Saini-Gaurav <107954342+Saini-Gaurav@users.noreply.github.com>
2025-05-05 18:17:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ mongoose
   });
 
 // Start the cron job
-cron.schedule("0 */8 * * *", () => {
+cron.schedule("0 */1 * * *", () => {
   console.log("Fetching and storing news...");
   fetchAndStoreNewsForAllCategories();
 });
