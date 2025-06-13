61ad2ac2c56ed4eb98d374e5e49c655c78dffd6e
Mridul-corazor <mridul@corazor.com>
2025-06-13 13:55:34 +0530

    Update flask_api.py

--- Chatbot_Backend/flask_api.py
+++ Chatbot_Backend/flask_api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from chatbot_logic import ArticleWriterModule, get_bot_response
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins":["http://localhost:5173","https://www.executepartners.com"]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins":["http://localhost:5173/","https://www.executepartners.com/"]}}, supports_credentials=True)
 SESSIONS = {}
 
 @app.route("/chat", methods=["POST","OPTIONS"])
