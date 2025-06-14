58c475b2dcc181bf5e3b0b32866397fc47f0829a
Corazor Technology Private Limited <153815210+Corazortechnology@users.noreply.github.com>
2025-06-14 11:31:33 +0530

    Update summaryapi.py

--- Chatbot_Backend/summaryapi.py
+++ Chatbot_Backend/summaryapi.py
@@ -119,6 +119,7 @@ CORS(app,
      resources={
          r"/*": {
              "origins": [ 
+                 "http://192.168.31.68:5500",
                  "http://localhost:5173", 
                  "http://127.0.0.1:5173",
                  "http://localhost:3000", 
